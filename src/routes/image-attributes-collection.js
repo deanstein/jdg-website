c106ee811276ae2741760769e152188bff4ab2f0
deanstein <jdeangoldstein@gmail.com>
2024-07-15 23:21:58 -0600

    Adding more imageAttributes

--- src/routes/image-attributes-collection.js
+++ src/routes/image-attributes-collection.js
@@ -1,9 +1,127 @@
 import { addCloudinaryUrlTransformation, instantiateObject } from 'jdg-ui-svelte/jdg-utils.js';
 import jdgImageAttributes from 'jdg-ui-svelte/schemas/jdg-image-attributes.js';
 
+// temporary - needs to be moved to jdg-ui-svelte
+// @ts-expect-error
+export const postProcessImageAttributes = (jdgImageAttributes) => {
+	// imgAlt is likely the same as imgCaption, unless already specified
+	if (jdgImageAttributes.imgCaption && !jdgImageAttributes.imgAlt) {
+		jdgImageAttributes.imgAlt = jdgImageAttributes.imgCaption;
+	}
+	return jdgImageAttributes;
+};
+
 // a map of all available images and their attributes
 // new images with a caption must be added here
 const imageAttributesCollection = {
+	arch: {},
+	exp: {
+		fms_campus_before_aerial: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721091660/jdg-website/exp/fms-campus-before-aerial.jpg'
+				),
+				imgCaption: 'An abandoned Flood Middle School is seen from above in 2013.'
+			})
+		),
+		fms_campus_before_1: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073616/jdg-website/exp/fms-campus-before-1.jpg'
+				),
+				imgCaption: 'Flood Middle School as it appeared from Broadway during demolition in 2013.'
+			})
+		),
+		fms_campus_before_2: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073611/jdg-website/exp/fms-campus-before-2.jpg'
+				),
+				imgCaption: 'The northwest corner of Flood Middle School during abandonment in 2013.'
+			})
+		),
+		fms_campus_before_3: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073613/jdg-website/exp/fms-campus-before-3.jpg'
+				),
+				imgCaption:
+					'The southeast corner of Flood Middle School at Lincoln and Kenyon as seen during abandonment in 2013.'
+			})
+		),
+		fms_campus_before_4: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073614/jdg-website/exp/fms-campus-before-4.jpg'
+				),
+				imgCaption:
+					'The northeast corner of Flood Middle School as seen during abandonment in 2013.'
+			})
+		),
+		fms_campus_aerial: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721090862/jdg-website/exp/fms-campus-aerial.jpg'
+				),
+				imgCaption:
+					'The main school building is repurposed to include walk-up townhomes in the central wing, restaurants in the former auditorium on the right, retail in the former gym on the left, and a new hotel in the back.'
+			})
+		),
+		fms_campus_lawn: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073527/jdg-website/exp/fms-campus-lawn.jpg'
+				),
+				imgCaption:
+					'The main building of the school is kept and repurposed to include walk-up townhomes in the central wing, restaurants in the former auditorium on the right, retail in the former gym on the left, and a new hotel in the back.'
+			})
+		),
+		fms_campus_kenyon_corner: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073526/jdg-website/exp/fms-campus-kenyon-corner.jpg'
+				),
+				imgCaption:
+					'At the intersection of Kenyon and Lincoln, the former middle school entrance becomes a common entrance for the back of the walk-up townhomes. The hotel can be seen on the right.'
+			})
+		),
+		fms_campus_massing: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721106934/jdg-website/exp/fms-campus-massing.jpg'
+				),
+				imgCaption:
+					'One possible reuse option is a mix of retail (red), office (green), townhomes (pink), apartments (teal), and hotel (gold).'
+			})
+		),
+		fms_campus_plaza: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073530/jdg-website/exp/fms-campus-plaza.jpg'
+				),
+				imgCaption:
+					'The former large gymnasium on the northwest corner is transformed into ground-level retail with offices above, along with a plaza to provide access to the east side of the property.'
+			})
+		),
+		fms_campus_paseo: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073528/jdg-website/exp/fms-campus-paseo.jpg'
+				),
+				imgCaption:
+					'A new hotel augments the existing building, providing nearly 100 rooms and adding scale to the northeast corner of the school. A new paseo on the north side provides a shady plaza for food trucks and informal outdoor dining.'
+			})
+		),
+		fms_campus_south_wing: postProcessImageAttributes(
+			instantiateObject(jdgImageAttributes, {
+				imgSrc: addCloudinaryUrlTransformation(
+					'https://res.cloudinary.com/jdg-main/image/upload/v1721073531/jdg-website/exp/fms-campus-south-wing.jpg'
+				),
+				imgCaption:
+					'On the south side of the school, an existing two-floor wing is transformed into walk-up townhomes.'
+			})
+		)
+	},
 	architecture_0: instantiateObject(jdgImageAttributes, {
 		imgSrc: addCloudinaryUrlTransformation(
 			'https://res.cloudinary.com/jdg-main/image/upload/v1720834645/jdg-website/architecture/architecture-0.jpg'
