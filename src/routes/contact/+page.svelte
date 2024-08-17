fa027955be1768d7bbae517294677385913f10f7
deanstein <jdeangoldstein@gmail.com>
2024-08-16 19:10:12 -0600

    Fleshing out Contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,7 +1,30 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer } from 'jdg-ui-svelte';
+	import { JDGBodyCopy, JDGContentBoxFloating, JDGContentContainer } from 'jdg-ui-svelte';
+	import SocialMedia from '../../components/SocialMedia.svelte';
 </script>
 
 <JDGContentContainer>
-	<JDGContentBoxFloating>CONTACT</JDGContentBoxFloating>
+	<JDGContentBoxFloating title={'GET IN TOUCH'}>
+		<JDGBodyCopy paddingTop="0" paddingBottom="0" textAlign="center">
+			Let's connect! You can reach me in a variety of ways:
+			<br />
+			<br />
+			<h3>SOCIAL MEDIA</h3>
+			<SocialMedia />
+			<br />
+			<br />
+			<h3>CONTACT FORM</h3>
+			<div style="display: flex; justify-content: center;">
+				<iframe
+					title="Contact Joshua Dean Goldstein"
+					src="https://docs.google.com/forms/d/e/1FAIpQLSdaDSTRnBD0pYsOtxQE21axuPDNuYbxvpCEoST2pNOV8y-4BQ/viewform?embedded=true"
+					width="640"
+					height="817"
+					frameborder="0"
+					marginheight="0"
+					marginwidth="0">Loading…</iframe
+				>
+			</div>
+		</JDGBodyCopy>
+	</JDGContentBoxFloating>
 </JDGContentContainer>
