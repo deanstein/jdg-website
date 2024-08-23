b6a07bf267347e7152bb2a6439ce510ae6a9b032
deanstein <jdeangoldstein@gmail.com>
2024-08-22 22:14:48 -0600

    Removing unused shared URL

--- src/lib/shared-strings.js
+++ src/lib/shared-strings.js
@@ -5,7 +5,4 @@ export const sharedUrls = {
 	hst: {
 		flood_middle_school: './architectural-history/flood-middle-school'
 	},
-	f505Design: 'https://www.505design.com',
-	forma: 'https://www.autodesk.com/in/products/forma/',
-	grasshopper: 'https://www.grasshopper3d.com/'
 };
