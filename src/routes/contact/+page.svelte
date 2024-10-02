1010a76d02d875548ad58d0c4142c905e4283167
deanstein <jdeangoldstein@gmail.com>
2024-10-02 00:00:22 -0600

    Using iFrame wrapper for Contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,17 @@
 <script>
-	import { JDGBodyCopy, JDGContentBoxFloating, JDGContentContainer } from 'jdg-ui-svelte';
+	import {
+		JDGBodyCopy,
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGIFrame
+	} from 'jdg-ui-svelte';
 	import SocialMedia from '../../components/SocialMedia.svelte';
 </script>
 
 <JDGContentContainer>
 	<JDGContentBoxFloating
 		title={'GET IN TOUCH'}
-		subtitle={'Say hello, ask a question, make a comment'}
+		subtitle={'Questions? Comments?'}
 	>
 		<JDGBodyCopy paddingTop="0" paddingBottom="0" textAlign="center">
 			<h3>SOCIAL MEDIA</h3>
@@ -14,17 +19,19 @@
 			<br />
 			<br />
 			<h3 style="margin-bottom: 10px;">CONTACT FORM</h3>
-			<div style="display: flex; justify-content: center;">
-				<iframe
-					title="Contact Joshua Dean Goldstein"
-					src="https://docs.google.com/forms/d/e/1FAIpQLSdaDSTRnBD0pYsOtxQE21axuPDNuYbxvpCEoST2pNOV8y-4BQ/viewform?embedded=true"
-					width="640"
-					height="817"
-					frameborder="0"
-					marginheight="0"
-					marginwidth="0">Loading…</iframe
-				>
-			</div>
 		</JDGBodyCopy>
+		<div class="iframe-wrapper">
+			<JDGIFrame
+				title={'Contact Joshua Dean Goldstein'}
+				src={'https://docs.google.com/forms/d/e/1FAIpQLSdaDSTRnBD0pYsOtxQE21axuPDNuYbxvpCEoST2pNOV8y-4BQ/viewform?embedded=true'}
+			/>
+		</div>
 	</JDGContentBoxFloating>
 </JDGContentContainer>
+
+<style>
+	.iframe-wrapper {
+		width: 100%;
+		margin-top: -40px;
+	}
+</style>
