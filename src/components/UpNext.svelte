fdc89f8c40313cebd7351e38863f95922eb61f8f
deanstein <jdeangoldstein@gmail.com>
2024-10-07 00:13:58 -0600

    Updating to jdg-ui-svelte v0.23.1 for objectPosition flag, setting it in one case

--- src/components/UpNext.svelte
+++ src/components/UpNext.svelte
@@ -10,6 +10,7 @@
 	item2Label={'ABOUT JOSH'}
 	item2Href="/about"
 	item2ImageAttributes={imageAttributesCollection.jdg_photo_2}
+	item2ObjectPosition="right"
 	item3Label="GET IN TOUCH"
 	item3Href="/contact"
 	item3ImageAttributes={imageAttributesCollection.exp.broadway_southwest_concept_2_promenade}
