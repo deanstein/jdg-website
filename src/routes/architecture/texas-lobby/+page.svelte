6ea45366d12cafe65a5077a10253c72e69a886b7
deanstein <jdeangoldstein@gmail.com>
2024-10-08 00:02:30 -0600

    Fixing image issue on Texas Lobby project

--- src/routes/architecture/texas-lobby/+page.svelte
+++ src/routes/architecture/texas-lobby/+page.svelte
@@ -8,6 +8,6 @@
 		title="MODERN TEXAS LOBBY"
 		subtitle="A contemporary take on Texas charm for a new tech office complex"
 	>
-		<JDGImage imageAttributes={imageAttributesCollection.arch.texas_lobby} maxHeight="600px" />
+		<JDGImage imageAttributes={imageAttributesCollection.arch.texas_lobby} maxHeight="600px" cropToFillContainer={false}/>
 	</JDGContentBoxFloating>
 </JDGContentContainer>
