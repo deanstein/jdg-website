ee5b87f3d4d2968a0d3edd83b206331152330aa0
deanstein <jdeangoldstein@gmail.com>
2024-10-08 00:05:41 -0600

    Another minor image sizing fix

--- src/routes/architecture/texas-lobby/+page.svelte
+++ src/routes/architecture/texas-lobby/+page.svelte
@@ -8,6 +8,11 @@
 		title="MODERN TEXAS LOBBY"
 		subtitle="A contemporary take on Texas charm for a new tech office complex"
 	>
-		<JDGImage imageAttributes={imageAttributesCollection.arch.texas_lobby} maxHeight="600px" cropToFillContainer={false}/>
+		<JDGImage
+			imageAttributes={imageAttributesCollection.arch.texas_lobby}
+			maxHeight="600px"
+			cropToFillContainer={false}
+			useCompactHeightOnMobile
+		/>
 	</JDGContentBoxFloating>
 </JDGContentContainer>
