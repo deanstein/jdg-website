90a9537cf309ea7ae58dd07a2ee90351e6f542a6
deanstein <jdeangoldstein@gmail.com>
2024-10-18 17:43:54 -0600

    Switching to hero image standard for Campus Village

--- src/routes/architectural-experiments/fms-campus/+page.svelte
+++ src/routes/architectural-experiments/fms-campus/+page.svelte
@@ -1,36 +1,45 @@
 <script>
+	import { pageMeta, sharedStyles } from '$lib/shared-strings';
+
 	import {
 		JDGBodyCopy,
 		JDGContentBoxFloating,
 		JDGContentContainer,
+		JDGFullWidthContainer,
 		JDGGridLayout,
 		JDGImageCarousel,
 		JDGImageFullWidth,
 		JDGImageTile
 	} from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
-	import { pageMeta } from '$lib/shared-strings';
+	import SeriesExperimental from '../../../components/SeriesExperimental.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title={pageMeta.exp.fms_campus.title.toUpperCase()}
-		subtitle={pageMeta.exp.fms_campus.description}
-	>
-		<JDGBodyCopy paddingTop="0" textAlign="center" fontSizeMultiplier={0.75}>
-			<i>
-				This project examines an alternate ending for an old building with a unique history that was
-				demolished in 2013.
-				<br />
-				For more on the original building, see my historical narrative on
-				<a href="/architectural-history/flood-middle-school">Flood Middle School</a>.
-				<br /><br />
-				This is part of a series of experimental 3D models
-				<br /> built while working on the
-				<a href="https://www.formit.autodesk.com/">Autodesk FormIt</a> software team.
-			</i>
-		</JDGBodyCopy>
-		<JDGImageFullWidth imageAttributes={imageAttributesCollection.exp.fms_campus_lawn} />
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText={pageMeta.exp.fms_campus.title.toUpperCase()}
+		primaryTextBold
+		secondaryText={pageMeta.exp.fms_campus.description}
+		imageAttributes={imageAttributesCollection.exp.fms_campus_lawn}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}>
+		<div class="notes">
+			<SeriesExperimental />
+			<JDGBodyCopy paddingTop="0" paddingBottom="0" textAlign="center" fontSizeMultiplier={0.75}>
+				<i>
+					This project examines an alternate ending for an old building with a unique history that
+					was demolished in 2013.
+					<br />
+					For more on the original building, see my historical narrative on
+					<a href="/architectural-history/flood-middle-school">Flood Middle School</a>.
+				</i>
+			</JDGBodyCopy>
+		</div>
 		<JDGBodyCopy>
 			The Campus Village concept is an exploration of alternate uses for the former <a
 				href="/architectural-history/flood-middle-school">Flood Middle School</a
@@ -64,39 +73,46 @@
 			anchored at the rear to the original school hallways, which would provide an interior common
 			circulation system.
 		</JDGBodyCopy>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.exp.fms_campus_lawn,
-				imageAttributesCollection.exp.fms_campus_before_1,
-				imageAttributesCollection.exp.fms_campus_kenyon_corner,
-				imageAttributesCollection.exp.fms_campus_before_3
-			]}
-			maxHeight="600px"
-		/>
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.exp.fms_campus_lawn,
+					imageAttributesCollection.exp.fms_campus_before_1,
+					imageAttributesCollection.exp.fms_campus_kenyon_corner,
+					imageAttributesCollection.exp.fms_campus_before_3
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 		<JDGBodyCopy>
 			Other spaces like the 2-story auditorium and gym on either end would be transformed into
 			restaurant incubator space, as well as worksharing and office space. The original timber
 			structure from both would be exposed to add character to the spaces.
 		</JDGBodyCopy>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.exp.fms_campus_plaza,
-				imageAttributesCollection.exp.fms_campus_before_2
-			]}
-			maxHeight="600px"
-		/>
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.exp.fms_campus_plaza,
+					imageAttributesCollection.exp.fms_campus_before_2
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 		<JDGBodyCopy>
 			Newer construction at the back of the school could support additional floors, and would become
 			a boutique hotel in this concept. The hotel would drive foot traffic around the project's
 			perimeter, including the new food truck-lined paseo that links the front and back of the
 			property.
 		</JDGBodyCopy>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.exp.fms_campus_paseo,
-				imageAttributesCollection.exp.fms_campus_before_4
-			]}
-			maxHeight="600px"
-		/>
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.exp.fms_campus_paseo,
+					imageAttributesCollection.exp.fms_campus_before_4
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
