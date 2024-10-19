97b4a0ba895eb42dc8d4be4eda4b93c6fe795eeb
deanstein <jdeangoldstein@gmail.com>
2024-10-18 22:10:00 -0600

    Updating Gritty BBQ with hero image and UpNext

--- src/routes/architecture/gritty-bbq/+page.svelte
+++ src/routes/architecture/gritty-bbq/+page.svelte
@@ -1,20 +1,41 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import {
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGFullWidthContainer,
+		JDGImageCarousel,
+		JDGImageFullWidth
+	} from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="GRITTY BARBECUE"
-		subtitle="Studies for transforming a derelict duplex into a new restaurant"
-	>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.arch.gritty_bbq_option_2,
-				imageAttributesCollection.arch.gritty_bbq_option_3,
-				imageAttributesCollection.arch.gritty_bbq_option_5
-			]}
-			maxHeight="600px"
-		/>
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="GRITTY BARBECUE"
+		primaryTextBold
+		secondaryText="Studies for transforming a derelict duplex into a new restaurant"
+		imageAttributes={imageAttributesCollection.arch.gritty_bbq_option_3}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}>
+		<Series505 />
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.arch.gritty_bbq_option_2,
+					imageAttributesCollection.arch.gritty_bbq_option_3,
+					imageAttributesCollection.arch.gritty_bbq_option_5
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
