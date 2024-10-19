dec9f99e8d68a72abba1bf51e1f34e8653915ec0
deanstein <jdeangoldstein@gmail.com>
2024-10-18 22:23:57 -0600

    Updating Old Orchard with hero image and UpNext

--- src/routes/architecture/old-orchard/+page.svelte
+++ src/routes/architecture/old-orchard/+page.svelte
@@ -1,20 +1,41 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import {
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGFullWidthContainer,
+		JDGImageCarousel,
+		JDGImageFullWidth
+	} from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="OLD ORCHARD"
-		subtitle="Architectural fabrication that reinforces a brand"
-	>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.arch.old_orchard_close,
-				imageAttributesCollection.arch.old_orchard_outside,
-				imageAttributesCollection.arch.old_orchard_covered
-			]}
-			maxHeight="600px"
-		/>
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="OLD ORCHARD"
+		primaryTextBold
+		secondaryText="Architectural fabrication that reinforces a brand"
+		imageAttributes={imageAttributesCollection.arch.old_orchard_outside}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}>
+		<Series505 />
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.arch.old_orchard_close,
+					imageAttributesCollection.arch.old_orchard_outside,
+					imageAttributesCollection.arch.old_orchard_covered
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
