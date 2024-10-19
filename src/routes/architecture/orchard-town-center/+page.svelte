0b5c9ca9f3ab264d8b1c504cbe52dac82dda82f0
deanstein <jdeangoldstein@gmail.com>
2024-10-18 22:29:37 -0600

    Updating Orchard Town Center with hero image and UpNext

--- src/routes/architecture/orchard-town-center/+page.svelte
+++ src/routes/architecture/orchard-town-center/+page.svelte
@@ -1,22 +1,43 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import {
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGFullWidthContainer,
+		JDGImageCarousel,
+		JDGImageFullWidth
+	} from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="ORCHARD TOWN CENTER"
-		subtitle="A community gathering space inspired by Prairie Style architecture"
-	>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.arch.otc_below,
-				imageAttributesCollection.arch.otc_far,
-				imageAttributesCollection.arch.otc_column,
-				imageAttributesCollection.arch.otc_seating,
-				imageAttributesCollection.arch.otc_rendering
-			]}
-			maxHeight="600px"
-		/>
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="ORCHARD TOWN CENTER"
+		primaryTextBold
+		secondaryText="A community gathering space inspired by nature"
+		imageAttributes={imageAttributesCollection.arch.otc_column}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}>
+		<Series505 />
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.arch.otc_below,
+					imageAttributesCollection.arch.otc_far,
+					imageAttributesCollection.arch.otc_column,
+					imageAttributesCollection.arch.otc_seating,
+					imageAttributesCollection.arch.otc_rendering
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
