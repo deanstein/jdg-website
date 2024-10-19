82bf69a55a18d6beb8365ee5a3f6840ed4dc6a9c
deanstein <jdeangoldstein@gmail.com>
2024-10-18 21:52:06 -0600

    Updating Bayou Mixed Use with hero image and UpNext

--- src/routes/architecture/bayou-mixed-use/+page.svelte
+++ src/routes/architecture/bayou-mixed-use/+page.svelte
@@ -1,21 +1,42 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import {
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGImageCarousel,
+		JDGFullWidthContainer,
+		JDGImageFullWidth
+	} from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="BAYOU MIXED USE"
-		subtitle="A new urban riverside destination for New Orleans"
-	>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.arch.bayou_mixed_use_4,
-				imageAttributesCollection.arch.bayou_mixed_use_2,
-				imageAttributesCollection.arch.bayou_mixed_use_3,
-				imageAttributesCollection.arch.bayou_mixed_use_1
-			]}
-			maxHeight="600px"
-		/>
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="BAYOU MIXED USE"
+		primaryTextBold
+		secondaryText="A new riverside destination"
+		imageAttributes={imageAttributesCollection.arch.bayou_mixed_use_4}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}>
+		<Series505 />
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.arch.bayou_mixed_use_4,
+					imageAttributesCollection.arch.bayou_mixed_use_2,
+					imageAttributesCollection.arch.bayou_mixed_use_3,
+					imageAttributesCollection.arch.bayou_mixed_use_1
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
