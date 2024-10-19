ffdd8d8b9f7803d2d1277e70258183204ccd7a10
deanstein <jdeangoldstein@gmail.com>
2024-10-18 22:14:50 -0600

    Updating Mindful Campus with hero image and UpNext

--- src/routes/architecture/mindful-campus/+page.svelte
+++ src/routes/architecture/mindful-campus/+page.svelte
@@ -1,13 +1,26 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel, JDGImageFullWidth } from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="MINDFUL LIVE/WORK CAMPUS"
-		subtitle="A sustainable, community-focused neighborhood"
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="MINDFUL LIVE/WORK CAMPUS"
+		primaryTextBold
+		secondaryText="A sustainable, community-focused neighborhood"
+		imageAttributes={imageAttributesCollection.arch.mindful_campus_boulevard_close}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}
 	>
+	<Series505 />
 		<JDGImageCarousel
 			imageAttributeObjects={[
 				imageAttributesCollection.arch.mindful_campus_boulevard_close,
@@ -18,4 +31,5 @@
 			maxHeight="600px"
 		/>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
