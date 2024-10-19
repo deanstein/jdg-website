b964933ca1f81c24a697d8a130d6a9ec54a8e3d0
deanstein <jdeangoldstein@gmail.com>
2024-10-18 22:20:37 -0600

    Updating Mindful Multifamilyl with hero image and UpNext

--- src/routes/architecture/mindful-multifamily/+page.svelte
+++ src/routes/architecture/mindful-multifamily/+page.svelte
@@ -1,13 +1,27 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import { JDGContentBoxFloating, JDGContentContainer, JDGFullWidthContainer, JDGImageCarousel, JDGImageFullWidth } from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="MINDFUL MULTIFAMILY"
-		subtitle="Multifamily for a cutting-edge tech/health/research campus"
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="MINDFUL MULTIFAMILY"
+		primaryTextBold
+		secondaryText="Multifamily for a cutting-edge tech/health/research campus"
+		imageAttributes={imageAttributesCollection.arch.mindful_multifamily}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}
 	>
+	<Series505 />
+	<JDGFullWidthContainer>
 		<JDGImageCarousel
 			imageAttributeObjects={[
 				imageAttributesCollection.arch.mindful_multifamily,
@@ -16,5 +30,7 @@
 			]}
 			maxHeight="600px"
 		/>
+	</JDGFullWidthContainer>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
