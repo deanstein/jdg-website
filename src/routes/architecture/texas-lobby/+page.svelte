113ff8160ea3500f1a92916996537361ae73601a
deanstein <jdeangoldstein@gmail.com>
2024-10-18 22:39:24 -0600

    Updating Texas Lobby with hero image and UpNext

--- src/routes/architecture/texas-lobby/+page.svelte
+++ src/routes/architecture/texas-lobby/+page.svelte
@@ -1,13 +1,26 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImage } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import { JDGContentBoxFloating, JDGContentContainer, JDGImage, JDGImageFullWidth } from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="MODERN TEXAS LOBBY"
-		subtitle="A contemporary take on Texas charm for a new tech office complex"
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="MODERN TEXAS LOBBY"
+		primaryTextBold
+		secondaryText="A contemporary take on Texas charm for a new tech office complex"
+		imageAttributes={imageAttributesCollection.arch.texas_lobby}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}
 	>
+	<Series505 />
 		<JDGImage
 			imageAttributes={imageAttributesCollection.arch.texas_lobby}
 			maxHeight="600px"
@@ -15,4 +28,5 @@
 			useCompactHeightOnMobile
 		/>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
