21d18e6f4890fbcb656758a4513ccf40898e9ae2
deanstein <jdeangoldstein@gmail.com>
2024-10-18 22:05:18 -0600

    Updating Fairgrounds with hero image and UpNext

--- src/routes/architecture/fairgrounds/+page.svelte
+++ src/routes/architecture/fairgrounds/+page.svelte
@@ -1,21 +1,42 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import {
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGFullWidthContainer,
+		JDGImageCarousel,
+		JDGImageFullWidth
+	} from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="THE FAIRGROUNDS"
-		subtitle="A new community on a former Worlds Fair site"
-	>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.arch.fairgrounds_aerial,
-				imageAttributesCollection.arch.fairgrounds_river,
-				imageAttributesCollection.arch.fairgrounds_fountain,
-				imageAttributesCollection.arch.fairgrounds_park
-			]}
-			maxHeight="600px"
-		/>
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="THE FAIRGROUNDS"
+		primaryTextBold
+		secondaryText="A new community on a former World's Fair site"
+		imageAttributes={imageAttributesCollection.arch.fairgrounds_aerial}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}>
+		<Series505 />
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.arch.fairgrounds_aerial,
+					imageAttributesCollection.arch.fairgrounds_river,
+					imageAttributesCollection.arch.fairgrounds_fountain,
+					imageAttributesCollection.arch.fairgrounds_park
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
