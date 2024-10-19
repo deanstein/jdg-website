bc87b1ca8c92fdc8fafd533e97a68406653f6a0e
deanstein <jdeangoldstein@gmail.com>
2024-10-18 21:59:34 -0600

    Updating Beach Chic with hero image and UpNext

--- src/routes/architecture/beach-chic/+page.svelte
+++ src/routes/architecture/beach-chic/+page.svelte
@@ -1,20 +1,41 @@
 <script>
-	import { JDGContentBoxFloating, JDGContentContainer, JDGImageCarousel } from 'jdg-ui-svelte';
 	import imageAttributesCollection from '../../image-attributes-collection';
+	import { sharedStyles } from '$lib/shared-strings';
+
+	import {
+		JDGContentBoxFloating,
+		JDGContentContainer,
+		JDGFullWidthContainer,
+		JDGImageCarousel,
+		JDGImageFullWidth
+	} from 'jdg-ui-svelte';
+	import Series505 from '../../../components/Series505.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="COASTAL DESTINATION ADAPTIVE REUSE"
-		subtitle="A historic block gets a beach chic makeover"
-	>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.arch.beach_chic_concourse,
-				imageAttributesCollection.arch.beach_chic_towers,
-				imageAttributesCollection.arch.beach_chic_aerial
-			]}
-			maxHeight="600px"
-		/>
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="COASTAL DESTINATION ADAPTIVE REUSE"
+		primaryTextBold
+		secondaryText="A historic block gets a beach chic makeover"
+		imageAttributes={imageAttributesCollection.arch.beach_chic_concourse}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}>
+		<Series505 />
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.arch.beach_chic_concourse,
+					imageAttributesCollection.arch.beach_chic_towers,
+					imageAttributesCollection.arch.beach_chic_aerial
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
