d261e68f38ef53b2e770321f9e2bc71853326acc
deanstein <jdeangoldstein@gmail.com>
2024-10-18 18:07:48 -0600

    Switching to hero image standard for Sixteen Pearl

--- src/routes/architectural-experiments/sixteen-pearl/+page.svelte
+++ src/routes/architectural-experiments/sixteen-pearl/+page.svelte
@@ -1,26 +1,33 @@
 <script>
+	import { sharedStyles } from '$lib/shared-strings';
+	import imageAttributesCollection from '../../image-attributes-collection';
+
 	import {
 		JDGBodyCopy,
 		JDGContentBoxFloating,
 		JDGContentContainer,
+		JDGFullWidthContainer,
 		JDGImageCarousel,
+		JDGImageFullWidth,
 		JDGImageTile
 	} from 'jdg-ui-svelte';
-	import imageAttributesCollection from '../../image-attributes-collection';
+	import SeriesExperimental from '../../../components/SeriesExperimental.svelte';
+	import UpNext from '../../../components/UpNext.svelte';
 </script>
 
-<JDGContentContainer>
-	<JDGContentBoxFloating
-		title="16 PEARL ADAPTIVE REUSE"
-		subtitle="Striking new townhomes emerge from a bland apartment block"
-	>
-		<JDGBodyCopy paddingTop="0" textAlign="center" fontSizeMultiplier={0.75}>
-			<i>
-				This is part of a series of experimental 3D models
-				<br /> built while working on the
-				<a href="https://www.formit.autodesk.com/">Autodesk FormIt</a> software team.
-			</i>
-		</JDGBodyCopy>
+<JDGContentContainer overlapWithHeader paddingTop="0" paddingBottom="0" gap="0">
+	<JDGImageFullWidth
+		primaryText="16 PEARL ADAPTIVE REUSE"
+		primaryTextBold
+		secondaryText="Striking new townhomes emerge from a bland apartment block"
+		imageAttributes={imageAttributesCollection.exp.sixteen_pearl_corner}
+		overlapWithHeader
+		overlayColorRgba={sharedStyles.overlayColorDarkRgba}
+	/>
+</JDGContentContainer>
+<JDGContentContainer paddingTop="0">
+	<JDGContentBoxFloating animateWhenVisible={false}>
+		<SeriesExperimental />
 		<JDGBodyCopy paddingTop="0">
 			This nondescript apartment block at the intersection of 16th Ave and Pearl St in Denver was
 			demolished in 2016.
@@ -30,14 +37,16 @@
 			deck spaces. The original mansard roof and exterior brick are both retained and painted, but trimmed
 			where necessary.
 		</JDGBodyCopy>
-		<JDGImageCarousel
-			imageAttributeObjects={[
-				imageAttributesCollection.exp.sixteen_pearl_corner,
-				imageAttributesCollection.exp.sixteen_pearl_before,
-				imageAttributesCollection.exp.sixteen_pearl_terrace
-			]}
-			maxHeight="600px"
-		/>
+		<JDGFullWidthContainer>
+			<JDGImageCarousel
+				imageAttributeObjects={[
+					imageAttributesCollection.exp.sixteen_pearl_corner,
+					imageAttributesCollection.exp.sixteen_pearl_before,
+					imageAttributesCollection.exp.sixteen_pearl_terrace
+				]}
+				maxHeight="600px"
+			/>
+		</JDGFullWidthContainer>
 		<JDGBodyCopy>
 			Inside, the townhomes feature open, airy spaces connected by a new modern stair tower. Channel
 			glass light wells bring natural glow to the townhomes and to the original common hallway on
@@ -50,4 +59,5 @@
 			showCaption={true}
 		/>
 	</JDGContentBoxFloating>
+	<UpNext />
 </JDGContentContainer>
