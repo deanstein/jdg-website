533c3006651f8de79c101f5f953f5e95947cca1c
deanstein <jdeangoldstein@gmail.com>
2025-12-07 19:47:27 -0700

    Add image-meta-registry.js for early jdg-ui-svelte v2.0 testing

---
+++ src/routes/image-meta-registry.js
@@ -0,0 +1,13 @@
+// TEMPORARY REGISTRY FOR TESTING AHEAD OF JDG-UI-SVELTE v2.0
+
+// This registry contains all available images and their metadata
+// New images with a caption must be added manually or via ImageMetaModal
+const imageMetaRegistry = {
+	reupload_test: {
+		src: 'https://res.cloudinary.com/jdg-main/image/upload/v1765161210/jdg-ui-svelte/image-testing/reupload-test.jpg',
+		alt: 'This is a reupload test!',
+		caption:
+			'This is a reupload test!',
+		attribution: 'Some attribution'
+	},
+}
